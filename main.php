ea90c91680acd30bdc037e3dae59c45bcefc8919
Patrick Romowicz <patrick@romowicz.de>
2013-09-12 23:22:02 +0200

    Google API sample, conf.yml and more

--- main.php
+++ main.php
@@ -1,3 +1,14 @@
 <?php
 
-echo 'NeverPass';
+// Composer Autoload
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Need conf.yml
+if(!file_exists(__DIR__.'/conf.yml')) throw new Exception('conf.yml is missing');
+
+// Load Config
+$yaml = new Symfony\Component\Yaml\Parser();
+$conf = $yaml->parse(file_get_contents(__DIR__.'/conf.yml'));
+
+// Google API Test
+require_once __DIR__ . '/sample/google-api-php-client.php';
