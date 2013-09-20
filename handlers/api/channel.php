7c7987cc3294a79d8db192664277cce6caf3b18e
Patrick Romowicz <patrick@romowicz.de>
2013-09-20 21:12:23 +0200

    Channel API updated

--- handlers/api/channel.php
+++ handlers/api/channel.php
@@ -6,22 +6,31 @@ require_once __DIR__ . '/../../bootstrap.php';
 $request = $container->getRequest();
 $user = $container->getCurrentUser();
 
+// Init Channel
 if ($channelId = $request->get('id')) {
     // Channel available
     $channel = \NeverPass\Channel::getCached($channelId);
-    $channel->addUser($user);
-
-    $location = new \NeverPass\Location(10, 20, 30, $user->getId());
-    $channel->addLocation($location);
-
-    $channel->save();
-    $response = new \Symfony\Component\HttpFoundation\JsonResponse($channel->toArray());
-    $response->send();
 } else {
-    // Channel init
     $channel = new \NeverPass\Channel();
-    $channel->addUser($user);
-    $channel->save();
-    $response = new \Symfony\Component\HttpFoundation\RedirectResponse($container->getUrl() . '/api/channel?id=' . $channel->getId());
-    $response->send();
 }
+
+// Add User
+$channel->addUser($user);
+
+// Init Location
+$longitude = $request->get('lon');
+$latitude = $request->get('lat');
+$heading = $request->get('hdg');
+
+if (strlen($longitude) && strlen($latitude) && strlen($heading)) {
+    $location = new \NeverPass\Location($heading, $latitude, $longitude, $user->getId());
+    $channel->addLocation($location);
+}
+// Save channel to Memcached
+$channel->save();
+
+// create response
+$data = $channel->toArray();
+$data['url'] = $container->getUrl() . '/api/channel?id=' . $channel->getId();
+$response = new \Symfony\Component\HttpFoundation\JsonResponse($data);
+$response->send();
