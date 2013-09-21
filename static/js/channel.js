f52bc720393d38c3fb11850a220295856952dd61
Patrick Romowicz <patrick@romowicz.de>
2013-09-21 14:14:43 +0200

    Channel JS API update

--- static/js/channel.js
+++ static/js/channel.js
@@ -5,18 +5,31 @@ var Location = function (heading, latitude, longitude, accuracy) {
     this.acc = accuracy;
 };
 
-var Channel = function (id, location) {
-    this.id = id == undefined ? '' : id;
-    this.location = location == undefined ? {} : location;
+var Channel = function (param) {
+    this.location = {};
+    this.id = '';
     this.url = '';
     this.users = [];
     this.locations = [];
+    this.intervall = null;
+
+    this.onUpdate = function () {
+    };
+
+    if (param instanceof Location) {
+        this.location = param;
+    } else if (param) {
+        this.id = param;
+    }
 
     // first init!
     this.update();
+
+    return this;
 };
 
 Channel.prototype.update = function () {
+    console.log('Channel update...');
     var data = {};
     if (this.id.length) {
         jQuery.extend(data, {id: this.id});
@@ -30,10 +43,31 @@ Channel.prototype.update = function () {
     })
         .done(jQuery.proxy(function (json) {
             jQuery.extend(this, json);
+            this.onUpdate(this);
         }, this));
 };
 
 Channel.prototype.setLocation = function (location) {
     location || (location = {});
     this.location = location;
+};
+
+Channel.prototype.start = function () {
+    this.stop();
+    this.intervall = window.setInterval(jQuery.proxy(function () {
+        this.update()
+    }, this), 5000);
+};
+
+Channel.prototype.stop = function () {
+    console.log('Channel stop..');
+    if (this.intervall != null) {
+        window.clearInterval(this.intervall);
+    }
+};
+
+Channel.prototype.onUpdate = function (func) {
+    if (typeof func == 'function') {
+        this.onUpdate = func;
+    }
 };
